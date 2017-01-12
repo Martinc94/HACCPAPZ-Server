4f32435fc581151e9d22788cfc25676bba3f8378
Martinc94 <martinc94@live.co.uk>
2017-01-12 18:02:22 +0000

    added classes

---
+++ WebServer/angular/app/classes/Transport/Transport.js
@@ -0,0 +1,8 @@
+"use strict";
+var Transport = (function () {
+    function Transport() {
+    }
+    return Transport;
+}());
+exports.Transport = Transport;
+//# sourceMappingURL=Transport.js.map
---
+++ WebServer/angular/app/classes/Transport/Transport.ts
@@ -0,0 +1,15 @@
+export class Transport {
+    _id: string;
+    email:string;
+    date: string;
+    food: string;
+    batch: string;
+    customer: string;
+    separation: string;
+    temp: string;
+    comment: string;
+    sign: string;
+    checkon: string;
+    managersign: string;
+    __v: number;
+}
